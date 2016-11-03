ddf2a577835d04f0cb0dc079e8439b59dd38086b
kaylielu <kaylielu8@gmail.com>
2016-11-03 00:34:43 -0700

    added eomjis?

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ app.post('/webhook/', function(req, res){
       let sender = event.sender.id
       if(event.message && event.message.text){
         let text = event.message.text
-        sendTextMessage(sender, "Text received, echo: " + text.substring(0,200))
+        sendTextMessage(sender, "How do you feel today?")
+        sendTextMessage(sender, "😀 😐 😟 " )
         sendGenericMessage(sender)
       }
     }
