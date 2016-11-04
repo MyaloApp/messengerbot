b49c4c6cd6e77994ec048aeb9406dfba2325d314
kaylielu <kaylielu8@gmail.com>
2016-11-03 17:33:50 -0700

    fixed

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.post('/webhook/', function(req, res){
       let sender = event.sender.id
       if(event.message && event.message.text){
         let text = event.message.text
+        console.log(text)
         sendDailyText(sender)
         //sendGenericMessage(sender)
       }
