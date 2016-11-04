d983e65c797561d172dceb70f8fcef5cbf6d0fae
kaylielu <kaylielu8@gmail.com>
2016-11-03 17:28:02 -0700

    fixed

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ app.post('/webhook/', function(req, res){
     let messaging_events = req.body.entry[0].messaging
     for(let i = 0; i < messaging_events.length; i++){
       let event = req.body.entry[0].messaging[i]
+      console.log(event.message.text)
+      console.log(event.message)
       let sender = event.sender.id
       if(event.message && event.message.text){
         let text = event.message.text
